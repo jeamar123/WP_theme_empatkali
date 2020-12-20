bae2989f9dd1128732edeb1e52f7a3c1db4847d3
jrran90 <jr.ran90@gmail.com>
2020-12-20 19:00:41 +0800

    added google analytics and other trackers

--- footer.php
+++ footer.php
@@ -165,6 +165,49 @@
   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
   crossorigin=""></script>
 
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133445843-1"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-133445843-1');
+</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window,document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '399006504230859'); 
+	fbq('track', 'PageView');
+</script>
+
+<!-- Global site tag (gtag.js) - Google Ads: 786153224 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-786153224"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'AW-786153224');
+</script>
+
+<noscript>
+	<img height="1" width="1" 
+	src="https://www.facebook.com/tr?id=399006504230859&ev=PageView
+	&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
+
+
 <script>
 	function ready(fn) {
 		if (document.readyState != 'loading'){
